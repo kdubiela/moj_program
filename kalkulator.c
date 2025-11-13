bb4cb3c1d94090215a6b6d68b6f9585f3bf1bf45
kdubiela <k.dubiela.145@studms.ug.edu.pl>
2025-11-13 19:43:40 +0100

    Program kalkulator

---
+++ kalkulator.c
@@ -0,0 +1,1 @@
+#include <stdio.h>\nint main() {\n int a = 10, b = 5;\n printf("a + b = %d\n", a + b);\n    printf("a - b = %d\n", a - b);\n printf("a * b = %d\n", a * b);\n printf("a / b = %d\n", a / b);\n }
